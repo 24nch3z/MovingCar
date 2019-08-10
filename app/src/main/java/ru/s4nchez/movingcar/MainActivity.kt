5a60ee2fb4bd3cf2a280127795f1c4f50c1cbec6
24nch3z <sanchezzzgg22@gmail.com>
2019-08-10 12:36:47 +0300

    Добавил подсказку

--- app/src/main/java/ru/s4nchez/movingcar/MainActivity.kt
+++ app/src/main/java/ru/s4nchez/movingcar/MainActivity.kt
@@ -2,11 +2,18 @@ package ru.s4nchez.movingcar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        car_view.setOnClickListener {
+            hint_view.animate()
+                    .setDuration(2000L)
+                    .alpha(0.0f)
+                    .start()
+        }
     }
 }
